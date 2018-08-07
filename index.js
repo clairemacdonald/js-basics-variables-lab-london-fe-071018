0c6aa15e6a3bf376c70c93c98e42e22fe9eff9e8
clairemacdonald <claire.macdonald@blis.com>
2018-08-07 19:13:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // Code your solution in this file!
 let companyName = 'Scuber';
-expect(function () { Scuber = 'specialCompany' }).to.throw(TypeError);
 
-let mostProfitableNeighborhood = 'Chelsea';
-expect(mostProfitableNeighborhood).to.equal('Chelsea');
+
 
 expect(companyCeo).to.equal('Susan Smith');
