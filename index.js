e72ea9493ae3285b5229a4bebe57d71776d12318
clairemacdonald <claire.macdonald@blis.com>
2018-08-07 19:12:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ let companyName = 'Scuber';
 expect(function () { Scuber = 'specialCompany' }).to.throw(TypeError);
 
 let mostProfitableNeighborhood = 'Chelsea';
+expect(mostProfitableNeighborhood).to.equal('Chelsea');
 
 expect(companyCeo).to.equal('Susan Smith');
